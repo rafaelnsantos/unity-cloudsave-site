2871b5ff68ef5152ad0adddbebc8be610ba4e770
Rafael Santos <rafaelnsantos1@gmail.com>
2018-02-15 21:35:04 -0200

    fix quotes algolia

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -66,8 +66,8 @@ const siteConfig = {
   // You may provide arbitrary config keys to be used as needed by your template.
   repoUrl: 'https://github.com/rafaelnsantos/unity-cloudsave',
   algolia: {
-    apiKey: "db51bcd5e513cfe52faa7ea59f2566b5",
-    indexName: "unity_cloud_save"
+    apiKey: 'db51bcd5e513cfe52faa7ea59f2566b5',
+    indexName: 'unity_cloud_save'
   },
 };
 
